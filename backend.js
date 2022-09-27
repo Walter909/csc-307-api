3349112393c40413a257b314de6fb82f42e98651
Walter Toriola <walte@Walters-MacBook-Pro.local>
2022-09-27 15:46:32 -0700

    Checkpoint reached

--- backend.js
+++ backend.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const app = express();
+//Change to 5000 at end
 const port = 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
