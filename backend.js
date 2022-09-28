83c27797dc218bef297bf7c54d5931859ad0d977
Walter Toriola <walte@Walters-MacBook-Pro.local>
2022-09-28 13:14:32 -0700

    Name and job should work

--- backend.js
+++ backend.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-//Change to 5000 at end
 const port = 4000;
 
 app.use(express.json());
@@ -35,9 +34,16 @@ const users = {
   ],
 };
 
+//Get user by name and job
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+
+  if (job != undefined && name != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -49,6 +55,56 @@ app.get("/users", (req, res) => {
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
+
+//Get user by id
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+    res.status(404).send("Resource not found.");
+  else {
+    result = { users_list: result };
+    res.send(result);
+  }
+});
+
+function findUserById(id) {
+  return users["users_list"].find((user) => user["id"] === id); // or line below
+  //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+//Add user
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(200).end();
+});
+
+function addUser(user) {
+  users["users_list"].push(user);
+}
+
+//delete user by id
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = deleteUserId(id);
+  if (result === undefined || result.length == 0) {
+    res.status(404).send("Resource not found.");
+  } else {
+    result = { users_list: result };
+    res.send(result);
+  }
+});
+
+function deleteUserId(id) {
+  return users.users_list.filter((user) => user.id !== id);
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
